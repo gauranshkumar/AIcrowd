77205a7481444258176bd22b28a87f29bc5d46f7
Devesh <devesh.prasad@ext.aicrowd.com>
2022-02-25 16:27:28 +0530

    Landing Notebook

--- app/javascript/src/components/molecules/LandingNotebookCard/index.tsx
+++ app/javascript/src/components/molecules/LandingNotebookCard/index.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 
 import styles from './landingNotebook.module.scss';
 
-const { mainWrapper, titleText, subWrapper, textWrapper, statusWrapper, time, imgWrapper, imgNotebook } = styles;
+const { mainWrapper, titleText, subWrapper, textWrapper, statusWrapper, time, imgWrapper } = styles;
 
 export type LandingNotebookCardProps = {
   title: string;
@@ -24,7 +24,6 @@ const LandingNotebookCard = ({ title, image, author, url }: LandingNotebookCardP
             <div className={subWrapper}>
               <div className={imgWrapper}>
                 <img style={{ display: 'block' }}
-                  className={imgNotebook}
                   src={image}
                   placeholder="blur"
                   blurDataURL="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=="
